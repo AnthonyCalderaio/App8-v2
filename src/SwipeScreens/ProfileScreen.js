9dcfcff370804ef301574be4513a21ebd82af170
Anthony Joseph Calderaio Jr <AnthonyCalderaio@gmail.com>
2018-11-29 14:51:48 -0500

    Tab template

---
+++ src/SwipeScreens/ProfileScreen.js
@@ -0,0 +1,71 @@
+
+//ErrorMessage: Unrecognized font family "Ionicons"
+//Solved:   $npm install react-native-vector-icons --save 
+//Solved... (THEN: react-native run-ios)
+import React, {Component} from 'react';
+import {StyleSheet,  View, Text} from 'react-native';
+
+
+
+//import profile from '/Users/anthony/Desktop/Manifest/App6/src/profileData/profile.js';
+class ProfileScreen extends React.Component {
+
+
+
+    constructor(props) {
+      super(props);
+  
+      // this.client = new ApolloClient({
+      //   uri: "https://w5xlvm3vzz.lp.gql.zone/graphql",
+      // });
+
+  
+      this.state = {
+        isLoadingComplete: false,
+      };
+    }
+
+
+  render() {
+    // if (!this.state.isLoadingComplete && !this.props.skipLoadingScreen) {
+    //   return (
+    //     <AppLoading
+    //       startAsync={this._loadResourcesAsync}
+    //       onError={this._handleLoadingError}
+    //       onFinish={this._handleFinishLoading}
+    //     />
+    //   );
+    // } else {
+    //   return (
+    //     <View style={styles.container}>
+    //       {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+    //       <ApolloProvider client={this.client}>
+    //         <profile />
+    //       </ApolloProvider>
+    //     </View>
+    //   );
+    // }
+
+    
+    return (
+      <View>
+        <Text>Profile View</Text>
+        </View>
+    );
+  }
+
+  
+}
+
+
+export default ProfileScreen;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  
+    
+}}
+);
---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,79 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import { createStackNavigator, createAppContainer } from "react-navigation";
+import { createBottomTabNavigator } from 'react-navigation'
+import ProfileScreen from '/Users/anthony/Desktop/Manifest/app8/src/SwipeScreens/ProfileScreen.js';
+import BusinessScreen from '/Users/anthony/Desktop/Manifest/app8/src/SwipeScreens/BusinessScreen.js';
+import ChatScreen from '/Users/anthony/Desktop/Manifest/app8/src/SwipeScreens/ChatScreen.js';
+import SocialScreen from '/Users/anthony/Desktop/Manifest/app8/src/SwipeScreens/SocialScreen.js';
+
+
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
+});
+
+
+class HomeScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <AppTabNavigator/>
+      </View>
+    );
+  }
+}
+const AppTabNavigator = createBottomTabNavigator({
+  ProfileTab: ProfileScreen,
+  BusinessTab: BusinessScreen,
+  ChatTab: ChatScreen,
+  SocialTab: SocialScreen
+  
+  // ,ChatTab: {
+  //     screen: ChatScreen
+  // },BusinessTab: {
+  //     screen: BusinessScreen
+  // },SocialTab: {
+  //     screen: SocialScreen
+  // }
+  // ,MapTab: {
+  //     screen: MapScreen
+  // }
+  // ,
+  // IndexTab: {
+  //     screen: IndexScreen
+  // }
+});
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+export default createAppContainer(AppTabNavigator);
